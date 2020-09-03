94133e862bf6ae84cc6e1ad15652e287a58e1818
henkrehorst <henkrehorst@outlook.com>
2020-09-03 16:39:37 +0200

    Revert "Merge branch 'update/symfony_5_upgrade'"
    This reverts commit c9d97e75bbaf20273cb65c3da66929585568a50d, reversing
    changes made to cae27466f83b963307014308184378bf5dd50bee.

--- src/Repository/PhpVersionRepository.php
+++ src/Repository/PhpVersionRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\PhpVersion;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method PhpVersion|null find($id, $lockMode = null, $lockVersion = null)
