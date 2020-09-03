ce211c33e9c5b69f61c90a15091bf0f2cf866c84
henkrehorst <henkrehorst@outlook.com>
2020-09-03 16:12:30 +0200

    fix doctrine imports and psr17 bug

--- src/Repository/PhpVersionRepository.php
+++ src/Repository/PhpVersionRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\PhpVersion;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method PhpVersion|null find($id, $lockMode = null, $lockVersion = null)
